34d6b7757e708d3881840e6ceab706fe043a83de
Akhil Johnson <akhil@squline.com>
2024-12-18 08:16:45 +0530

    Added few docs

--- src/components/CodingDocs.vue
+++ src/components/CodingDocs.vue
@@ -1,8 +1,33 @@
 <template>
+
+  <a href="https://htmlcheatsheet.com/">htmlcheatsheet.com</a>
+  
   <h1>
     Docs
   </h1>
+  <p>
+    Camel Notation :- oneTwoThree
+  </p>
+  <p>
+    Pascal Notation :- OneTwoThree
+  </p>
+
+
+<h1>
+  JS
+</h1>
+
+<code>
+  <script type="text/javascript"> </script>
+</code>
+
+<div v-html="html_content"></div>
+
 </template>
 
-Camel Notation :- oneTwoThree
-Pascal Notation :- OneTwoThree
+
+<script>
+export default {
+  html_content: '<span>Akhil</span>',
+}
+</script>
