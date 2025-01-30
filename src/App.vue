9ced67fdfd679342283c062978f86129f2e29cc4
akhiljohnson2000 <akhiljohnson2000@gmail.com>
2025-01-30 08:48:13 +0530

    Added Motivation quotes

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,9 @@ body{
 
 <template>
   <p>
-    <router-link to="/docs">Docs</router-link> |
+    <!-- <router-link to="/docs">Docs</router-link> |
     <router-link to="/workout">Workouts</router-link> | 
-    <router-link to="/cool-works">SomeCoolWorks</router-link> 
+    <router-link to="/cool-works">SomeCoolWorks</router-link>  -->
     <br/>
   </p>
   <router-view></router-view>
